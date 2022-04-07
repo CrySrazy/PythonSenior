091c4d47602ea844291ac687a898d9cf917700c9
unknown <kirilmaluhin@gmail.com>
2022-04-07 16:58:50 +0300

    Dz2

---
+++ Leson 2/Dz2.py
@@ -0,0 +1,16 @@
+a = input("Ведіть перше число")
+b = input('Ведіть друге число')
+c = input('Ведіть дію')
+if c == '+':
+    print('a + b =' ,str(int(a) + int(b)))
+elif c == '-':
+    print('a - b =' ,str(int(a) - int(b)))
+elif c == '*':
+    print('a * b =' ,str(int(a) * int(b)))
+elif c == '/':
+    if b == '0':
+        print("ERROR")
+    else: 
+        print('a / b =' ,str(int(a) / int(b)))
+
+    
