8f1dc49e422bf3aba6fe1c7f35926387dc846ce7
unknown <kirilmaluhin@gmail.com>
2022-04-07 18:11:36 +0300

    Dz3

---
+++ Leson 3/fg 3.py
@@ -0,0 +1,27 @@
+#1 завдання
+number = input('Скільки разів')
+sum = 0
+for i in range(int(number)):
+    sum = sum + i
+    print(sum)
+
+print('Suma = ',str(sum))
+
+#2 завдання
+
+number = input('Скільки разів')
+sum = 0
+for i in range(int(number)):
+    sum =  i*i
+    print(sum)
+
+print('Suma = ',str(sum))
+
+#3 завдання
+number = input('Скільки разів')
+sum = 0
+for i in range(int(number)):
+    sum = sum + i*i
+    print(sum)
+
+print('Suma = ',str(sum))
